36ae044889f3c968b71bfbd811311908b3b955cc
FluffyRia <fluffyriabusiness@gmail.com>
2023-04-04 02:07:51 +0300

    #1 Match Board
    Created base of the board and loaded all the textures and GUI for future UI

---
+++ Assets/Scripts/MatchBoard.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchBoard : MonoBehaviour
+{
+    [SerializeField]
+    private int _width, _height;
+    private GameObject tilePrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
